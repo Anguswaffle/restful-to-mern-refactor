b8921246ea50e8c1666fc37f3b90b8599cbafd3e
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-06 23:43:00 -0800

    added getUser resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,13 @@
 const { AuthenticatinError } = require('apollo-server-express');
 const { Book, User } = require('../models');
-const {signToken} = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-
-};
+  Query: {
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId })
+    },
+  },
+}
 
 module.exports = resolvers;
