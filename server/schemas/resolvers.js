ae7f3221706d42b6a5e4148a0e7f1384a9852dbb
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-07 23:31:19 -0800

    query me resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,23 @@
-const { AuthenticatinError } = require('apollo-server-express');
+const { AuthenticatinError, AuthenticationError } = require('apollo-server-express');
 const { Book, User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
+  // Retrieves a single user 
   Query: {
     user: async (parent, { userId }) => {
       return User.findOne({ _id: userId })
     },
+
+    me: async (parent, args, context) => {
+      if(context.user) {
+        return Profile.findOne({_id: context.user._id});
+      }
+      throw new AuthenticationError('You need to be logged in!')
+    }
   },
+
+
 }
 
 module.exports = resolvers;
