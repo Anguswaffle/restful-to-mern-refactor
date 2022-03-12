b109298ae65068c327e31ce4a2670e60c3b303b7
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-11 18:25:02 -0800

    moved google search query

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
+  // GoogleBooks search query
+  const searchGoogleBooks = (query) => {
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  };
+  
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
